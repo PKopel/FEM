3e3ca1c5cd51ee705d9f0f1e8d1b957cd8430c41
PKopel <pawel.kopel2@gmail.com>
2020-01-25 15:09:07 +0100

    split to Utils and FEM

--- src/FEM.hs
+++ src/FEM.hs
@@ -1,17 +1,8 @@
-module Lib
-    ( someFunc
-    ) where
+module FEM(
+    solve
+) where
 
-type Func = (Double -> Double)
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
-partitions :: Double -> Double -> [Double] -> [Double]
-partitions d start = map (\x -> start + d * x)
-
-integral :: Func -> Double -> Double -> Double
-integral f a b = ((b-a)/20 *) $ foldl1 (+) [ f x | x <- partitions d a [1,2..20]] where d = (b-a)/20
+import Utils
 
 e :: Double -> Double -> Double -> Double
 e xk d x
@@ -25,9 +16,6 @@ e' xk d x
   | x >= xk && x < xk + d = -1 / d
   | otherwise = 0
 
-(#) :: Func -> Func -> Func
-f # g = \x -> (f x) * (g x)
-
 bij :: Func -> Func -> Func -> Double -> Double -> Double -> Double -> Double
 bij a b c xi xj d k = let u = e xi d
                           u' = e' xi d
@@ -39,20 +27,6 @@ bij a b c xi xj d k = let u = e xi d
 
 li :: Func -> Double -> Double -> Double -> Double
 li f xi d k = integral (f # e xi d) (xi - d) (xi + d) - k * (e xi d 0)
-
-thomas :: [Double] -> [Double] -> [Double] -> [Double] -> [Double]
-thomas as bs cs ds = xs
-              where n = length bs
-                    a i = as !! i
-                    b i = bs !! i
-                    c i = cs !! i
-                    d i = ds !! i
-                    x i = xs !! i
-                    b' i = bs' !! i
-                    d' i = ds' !! i
-                    bs' = b(0) : [b(i) - a(i)/b'(i-1) * c(i-1) | i <- [1..n-1]]
-                    ds' = d(0) : [d(i) - a(i)/b'(i-1) * d'(i-1) | i <- [1..n-1]]
-                    xs = reverse $ d'(n-1) / b'(n-1) : [(d'(i) - c(i) * x(i+1)) / b'(i) | i <- [n-2, n-3..0]]
 
 bijs :: Int -> Double -> Func -> Func -> Func -> Double -> [Double]
 bijs n nd a b c k =  [bij a b c (xks !! i) (xks !! (i + 1)) (1/nd) k | i <- [0..n-2]] ++ [0.0]
---
+++ src/Utils.hs
@@ -0,0 +1,32 @@
+module Utils
+    ( integral
+    , thomas
+    , partitions
+    , (#)
+    , Func
+    ) where
+
+type Func = (Double -> Double)
+
+partitions :: Double -> Double -> [Double] -> [Double]
+partitions d start = map (\x -> start + d * x)
+
+integral :: Func -> Double -> Double -> Double
+integral f a b = ((b-a)/30 *) $ foldl1 (+) [ f x | x <- partitions d a [1,2..30]] where d = (b-a)/30
+
+(#) :: Func -> Func -> Func
+f # g = \x -> (f x) * (g x)
+
+thomas :: [Double] -> [Double] -> [Double] -> [Double] -> [Double]
+thomas as bs cs ds = xs
+              where n = length bs
+                    a i = as !! i
+                    b i = bs !! i
+                    c i = cs !! i
+                    d i = ds !! i
+                    x i = xs !! i
+                    b' i = bs' !! i
+                    d' i = ds' !! i
+                    bs' = b(0) : [b(i) - a(i)/b'(i-1) * c(i-1) | i <- [1..n-1]]
+                    ds' = d(0) : [d(i) - a(i)/b'(i-1) * d'(i-1) | i <- [1..n-1]]
+                    xs = reverse $ d'(n-1) / b'(n-1) : [(d'(i) - c(i) * x(i+1)) / b'(i) | i <- [n-2, n-3..0]]
