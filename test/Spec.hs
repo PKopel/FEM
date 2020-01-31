0515598cc2406182d9b55a3451515d68cb619fd8
PKopel <pawel.kopel2@gmail.com>
2020-02-01 00:06:22 +0100

    add tests for partitions and integral

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,23 @@
 import Test.QuickCheck
 import Utils
 
-prop_solveM :: Int -> Bool
-prop_solveM x = solveM zeroes ones zeroes ones == ones
-        where n = if x > 2 then x else 3
-              ones = take n $ repeat 1
-              zeroes = take n $ repeat 0
+prop_solveM :: Int -> Property
+prop_solveM n = 2 < n ==> 
+        let ones = take n $ repeat 1
+            zeroes = take n $ repeat 0
+        in solveM zeroes ones zeroes ones == ones
+
+prop_partitions :: Double -> Double -> [Double] -> Bool
+prop_partitions d start list = partitions d start list == [d*x + start | x <- list]
+
+prop_integral :: Double ->  Property
+prop_integral p = p >= 1 ==> abs ((integral (\x -> x ** p) 0 1) - (1 / (p + 1))) < 0.1
 
 main :: IO ()
 main = do
   putStrLn "\n*** Testing solveM... ***"
   quickCheck prop_solveM
+  putStrLn "\n*** Testing partitions... ***"
+  quickCheck prop_partitions
+  putStrLn "\n*** Testing integral... ***"
+  quickCheck prop_integral
